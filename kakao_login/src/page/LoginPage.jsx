d6e2d0d57af516d32e28b2a74a8e50e9f1c1727a
Hyo Bin <a032875@gmail.com>
2024-08-06 21:06:01 +0900

    카카오로그인 js방식으로 변경하여 코드 적용

--- kakao_login/src/page/LoginPage.jsx
+++ kakao_login/src/page/LoginPage.jsx
@@ -1,14 +1,60 @@
 import React from "react";
-const K_REST_API_KEY = process.env.REACT_APP_K_REST_API_KEY;
-const K_REDIRECT_URI = "http://localhost:3000/oauth";
-const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${K_REST_API_KEY}&redirect_uri=${K_REDIRECT_URI}&response_type=code`;
+import { useEffect } from "react";
+// const K_REST_API_KEY = process.env.REACT_APP_K_REST_API_KEY;
+// const K_REDIRECT_URI = "http://localhost:3000/oauth";
+const K_JS_API_KEY = process.env.REACT_APP_K_JS_API_KEY;
+// const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${K_REST_API_KEY}&redirect_uri=${K_REDIRECT_URI}&response_type=code`;
 const LoginPage = () => {
-  const handleKakaoLogin = () => {
-    window.location.href = kakaoURL;
+  useEffect(() => {
+    // 카카오 SDK 초기화
+    if (window.Kakao && !window.Kakao.isInitialized()) {
+      window.Kakao.init(K_JS_API_KEY); // 카카오 JavaScript 키로 초기화
+    }
+  }, []);
+  const handleLogin = async () => {
+    try {
+      await new Promise((resolve, reject) => {
+        window.Kakao.Auth.login({
+          success: resolve,
+          fail: reject,
+        });
+      });
+
+      const response = await new Promise((resolve, reject) => {
+        window.Kakao.API.request({
+          url: "/v2/user/me",
+          success: resolve,
+          fail: reject,
+        });
+      });
+
+      const { id, kakao_account } = response;
+      const { profile } = kakao_account;
+
+      const userData = {
+        id: id,
+        nickname: profile.nickname,
+        profile_image: profile.profile_image_url,
+      };
+      console.log(userData);
+
+      const result = await fetch("http://your-backend-api.com/kakao-login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const data = await result.json();
+      console.log("Success:", data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
   return (
     <div>
-      <button onClick={handleKakaoLogin}>카카오톡 로그인</button>
+      <button onClick={handleLogin}>카카오톡 로그인</button>
     </div>
   );
 };
